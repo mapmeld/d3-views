fefbf12fe023d65555ccfed23941d5000a9833a5
Nick Doiron <ndoiron@mapmeld.com>
2020-03-22 16:37:30 -0400

    add demo data

--- preview.js
+++ preview.js
@@ -28,10 +28,14 @@ const places = {
   }
 };
 
-fetch("./mass_2018_bg.topojson").then(res => res.json()).then((tj) => {
-  const gj = topojson.feature(tj, tj.objects.mass_2018_bg);
+let gj = null,
+    ma_data = null,
+    selection = null;
+function paintMap() {
   Object.keys(places).forEach((place) => {
     document.querySelector("#" + place + "_map").innerHTML = "";
+  });
+  Object.keys(places).forEach((place) => {
     let my_svg = d3.select("#" + place + "_map")
       .append("svg")
       .attr("width", width)
@@ -44,7 +48,140 @@ fetch("./mass_2018_bg.topojson").then(res => res.json()).then((tj) => {
     	.style("stroke", "#000")
     	.style("stroke-width", 0.2)
     	.style("fill", (d) => {
-        return "#ccc";
+        let id = d.properties.GEOID;
+        if (!selection || selection === "none" || !ma_data) {
+          return "#ccc";
+        } else {
+          let total_pop = ma_data[id].total_pop,
+              tract_id = id.substring(0, id.length - 1),
+              pct = 0, sum = 0;
+          if (selection === "english") {
+            Object.keys(ma_data[id])
+              .filter(key => key.includes("some_english") || key.includes("no_english"))
+              .forEach(key => { pct += ma_data[id][key] });
+            pct /= total_pop;
+            if (pct < 0.1) {
+              return "#fff";
+            } else if (pct < 0.2) {
+              return "#edf8e9";
+            } else if (pct < 0.3) {
+              return "#bae4b3";
+            } else if (pct < 0.4) {
+              return "#74c476";
+            } else {
+              return "#238b45";
+            }
+          } else if (selection === "grandchildren") {
+            sum = ma_data[tract_id]["grandparent_grandchildren"];
+            if (sum < 10) {
+              return "#fff";
+            } else if (sum < 20) {
+              return "#eff3ff";
+            } else if (sum < 30) {
+              return "#bdd7e7";
+            } else if (sum < 40) {
+              return "#6baed6";
+            } else {
+              return "#2171b5";
+            }
+          } else if (selection === "poverty_percent") {
+            let tract_pop = ma_data[tract_id].total_pop;
+            sum = ma_data[tract_id]["poverty_total"];
+            pct = sum / tract_pop;
+            if (pct < 0.08) {
+              return "#fff";
+            } else if (pct < 0.16) {
+              return "#f2f0f7";
+            } else if (pct < 0.24) {
+              return "#cbc9e2";
+            } else if (pct < 0.32) {
+              return "#9e9ac8";
+            } else {
+              return "#6a51a3";
+            }
+          } else if (selection === "poverty_seniors_percent") {
+            let elder_sum = 0;
+            for (let bg = 0; bg < 10; bg++) {
+              let bgid = tract_id + String(bg);
+              if (ma_data[bgid]) {
+                Object.keys(ma_data[bgid])
+                  .filter(key => key.includes("65_66") || key.includes("67_69")
+                    || key.includes("70_74")
+                    || key.includes("75_79")
+                    || key.includes("80_84")
+                    || key.includes("85_plus"))
+                  .forEach(key => { elder_sum += ma_data[bgid][key] });
+              }
+            }
+            Object.keys(ma_data[tract_id])
+              .filter(key => ["poverty_m_65_74", "poverty_m_75_plus", "poverty_f_65_74", "poverty_f_75_plus"].includes(key))
+              .forEach(key => { sum += ma_data[tract_id][key] });
+            pct = sum / elder_sum;
+            if (pct < 0.1) {
+              return "#fff";
+            } else if (pct < 0.2) {
+              return "#eff3ff";
+            } else if (pct < 0.3) {
+              return "#bdd7e7";
+            } else if (pct < 0.4) {
+              return "#6baed6";
+            } else {
+              return "#2171b5";
+            }
+          } else if (selection === "poverty_kids") {
+            Object.keys(ma_data[tract_id])
+              .filter(key => key.includes("poverty_m_kids") || key.includes("poverty_f_kids"))
+              .forEach(key => { sum += ma_data[tract_id][key] });
+            if (sum < 100) {
+              return "#fff";
+            } else if (sum < 200) {
+              return "#eff3ff";
+            } else if (sum < 300) {
+              return "#bdd7e7";
+            } else if (sum < 400) {
+              return "#6baed6";
+            } else {
+              return "#2171b5";
+            }
+          } else if (selection === "over_65") {
+            Object.keys(ma_data[id])
+              .filter(key => key.includes("65_66") || key.includes("67_69")
+                || key.includes("70_74")
+                || key.includes("75_79")
+                || key.includes("80_84")
+                || key.includes("85_plus"))
+              .forEach(key => { pct += ma_data[id][key] });
+            pct /= total_pop;
+            if (pct < 0.1) {
+              return "#fff";
+            } else if (pct < 0.2) {
+              return "#eff3ff";
+            } else if (pct < 0.3) {
+              return "#bdd7e7";
+            } else if (pct < 0.4) {
+              return "#6baed6";
+            } else {
+              return "#2171b5";
+            }
+          }
+        }
       });
+  });
+}
+
+fetch("./mass_2018_bg.topojson").then(res => res.json()).then((tj) => {
+  gj = topojson.feature(tj, tj.objects.mass_2018_bg);
+  paintMap();
+
+  fetch("./ma_data.json").then(res => res.json()).then((blockgroups) => {
+    ma_data = blockgroups;
+
+    let dropdown = document.querySelector("select");
+    dropdown.disabled = false;
+    dropdown.onchange = (e) => {
+      selection = e.target.value;
+      console.log(selection);
+      paintMap();
+    };
   });
 });
